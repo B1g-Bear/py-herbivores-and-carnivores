8ad896b27a99b5e46dfddbfc8de00ff6c84121d7
Andrii Novikov <forytube2023@gmail.com>
2025-06-30 14:08:36 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,42 @@
-# write your code here
+class Animal:
+    alive: list["Animal"] = []
+
+    def __init__(
+        self,
+        name: str
+    ) -> None:
+        self.name: str = name
+        self.health: int = 100
+        self.hidden: bool = False
+        Animal.alive.append(self)
+
+    def __str__(self) -> str:
+        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def _die(self) -> None:
+        if self in Animal.alive:
+            Animal.alive.remove(self)
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(
+        self,
+        victim: Animal
+    ) -> None:
+        if not isinstance(victim, Herbivore):
+            return
+        if victim.hidden:
+            return
+
+        victim.health -= 50
+        if victim.health <= 0:
+            victim.health = 0
+            victim._die()
